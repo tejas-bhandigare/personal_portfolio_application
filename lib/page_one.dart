9d3815430d1f243a7d16d1c2f70095073b7e4363
Tejash Bhandigare <tejasbhandigare@gmail.com>
2024-08-06 02:57:38 +0530

    Working on subpages information and formating

--- lib/page_one.dart
+++ lib/page_one.dart
@@ -8,10 +8,102 @@ class PageOne extends StatelessWidget {
         title: Text('About me'),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: Text('This is Page One'),
-      ),
+      body:SingleChildScrollView(
+child:
+      Center(
 
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.start,
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: [
+           Container(
+             child:
+           Image.asset("assets/images/tejaslap.jpeg",
+           height:300 ,
+             width: 300,
+           ),
+           ),
+           Container(
+             height: 30,
+             width: 50,
+           ),
+
+           Container(
+             child:
+           Text("NAME - Tejas Bhaskar Bhandigare",
+           style: TextStyle(
+             fontSize: 20
+           ),
+           ),
+           ),
+
+           Container(
+             height: 30,
+             width: 50,
+           ),
+
+           Container(
+             child:Center(
+               child:
+               Text("Qualification - BCA Graduate (2023-24) ",
+                 style: TextStyle(
+                     fontSize: 20
+                 ),
+               ),
+             ),
+           ),
+
+           Container(
+             height: 30,
+             width: 50,
+           ),
+
+           Container(
+             child:Center(
+               child:
+               Text("Carrer Objectives -""To pursue a career with an organization will help me to explore and achieve expertise through opportunities and learning which will help me to enhance my skills along with ensuring organizational Success.",
+               style: TextStyle(
+                 fontSize: 20
+               ),),
+           ),
+           ),
+
+           Container(
+             height: 30,
+             width: 50,
+           ),
+
+
+
+           Container(
+             child:Center(
+               child:
+               Text("#Intrest -                                                      "
+                   " 1.Learning new things in tech                                  "
+                   " 2.Playing outdoor games                                        "
+                   " 3.Watching movies                                              "
+                   " 4.DO Challenging tasks                                         "
+                 ,
+                 style: TextStyle(
+                     fontSize: 20
+                 ),),
+             ),
+           ),
+
+
+           Container(
+             height: 30,
+             width: 50,
+           ),
+
+
+         ],
+
+
+       ),
+
+      ),
+      ),
     );
   }
 }
--- lib/page_two.dart
+++ lib/page_two.dart
@@ -1,15 +1,92 @@
 import 'package:flutter/material.dart';
 
+
 class PageTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Educcation'),
-      ),
-      body: Center(
-        child: Text('This is Page Two'),
-      ),
+    return
+       Scaffold(
+        appBar: AppBar(
+          title: Text('Education Details '),
+          backgroundColor: Colors.blueAccent,
+        ),
+
+ body: ListView(
+
+   padding: const EdgeInsets.all(8),
+   children: <Widget>[
+     ListTile( title: Text("Bacholore of Computer Application (2023-24)",
+     style: TextStyle(
+       fontSize: 20
+     ),
+     )
+
+         ,subtitle: Text("Dattajirao Kadam Arts, Science and Commerce College, Ichalkaranji         "
+             "Shivaji Univercity,Kolhapur                                               "
+
+         "CGPA - 8.72",
+
+           style: TextStyle(
+             fontSize: 18
+           ),
+
+         )
+     ),
+
+Container(
+  height: 30,
+  width: 60,
+),
+
+
+     ListTile( title: Text("Higher Secondary (2020) ",
+     style: TextStyle(
+       fontSize: 20
+     ),),
+         subtitle: Text("KISANRAO MORE HIGH SCHOOL AND JUNIOR COLLEGE SARAWADE                                             "
+             "Percentage - 70.20",
+           style: TextStyle(
+             fontSize: 18
+           ),
+         )),
+     Container(
+       height: 30,
+       width: 60,
+     ),
+
+
+     ListTile( title: Text("Secondary School (2020)",
+     style: TextStyle(
+       fontSize: 20
+     ),
+     ),
+
+         subtitle: Text("Warana Vidyaniketan , Nave Pargoan  "
+             "Percentage - 75.20",
+        style: TextStyle(
+          fontSize: 18
+        ),
+         )),
+     Container(
+       height: 30,
+       width: 60,
+     ),
+
+
+   ],
+
+ ),
+
+
+
+
     );
   }
 }
+
+
+
+  
+
+
+
